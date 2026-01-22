144a8237a012570e626ff943f90742508239ed9d
daniel <danielngun04@gmail.com>
2026-01-22 15:23:52 +0200

    Add JwtUtils, UserDetailsImpl, and UserDetailsServiceImpl classes for JWT authentication and user details management

---
+++ simccs/src/main/java/com/acp/simccs/common/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.acp.simccs.common.security;
+
+import com.acp.simccs.modules.identity.model.User; // Error until Module A
+import com.acp.simccs.modules.identity.repository.UserRepository; // Error until Module A
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
+
+        return UserDetailsImpl.build(user);
+    }
+}
