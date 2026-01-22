1dc81131ba5094c783980394140f34cd30251eca
daniel <danielngun04@gmail.com>
2026-01-22 15:30:33 +0200

    Add RoleRepository for role data access with name lookup method

---
+++ simccs/src/main/java/com/acp/simccs/modules/identity/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.acp.simccs.modules.identity.repository;
+
+import com.acp.simccs.modules.identity.model.ERole;
+import com.acp.simccs.modules.identity.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(ERole name);
+}
