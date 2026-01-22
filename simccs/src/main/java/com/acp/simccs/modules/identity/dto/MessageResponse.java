0d1e09de9ad4eb284e252e65b12c0966e38892d5
daniel <danielngun04@gmail.com>
2026-01-22 15:33:32 +0200

    Add MessageResponse DTO for standardized message handling

---
+++ simccs/src/main/java/com/acp/simccs/modules/identity/dto/MessageResponse.java
@@ -0,0 +1,10 @@
+package com.acp.simccs.modules.identity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
