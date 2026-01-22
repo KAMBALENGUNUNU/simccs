26e7ae75a4ae0185b05128f95733ed8a747769b0
daniel <danielngun04@gmail.com>
2026-01-22 16:41:53 +0200

    Add ChatChannelRepository for managing chat channel data

---
+++ simccs/src/main/java/com/acp/simccs/modules/communication/repository/ChatChannelRepository.java
@@ -0,0 +1,12 @@
+package com.acp.simccs.modules.communication.repository;
+
+import com.acp.simccs.modules.communication.model.ChatChannel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ChatChannelRepository extends JpaRepository<ChatChannel, Long> {
+    Optional<ChatChannel> findByName(String name);
+}
