92fdddc34251c4e6120b64779e507e8f1b9a1d56
daniel <danielngun04@gmail.com>
2026-01-22 15:44:20 +0200

    Add Category entity for crisis management with unique name constraint

---
+++ simccs/src/main/java/com/acp/simccs/modules/crisis/model/Category.java
@@ -0,0 +1,22 @@
+package com.acp.simccs.modules.crisis.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+}
