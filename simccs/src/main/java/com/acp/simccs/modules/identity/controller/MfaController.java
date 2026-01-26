e3fb6fd3b43515879fa20398bff7392063cb5cb2
daniel <danielngun04@gmail.com>
2026-01-26 14:25:53 +0200

    added the configuirations for swagger to get all my apis for testing

--- simccs/src/main/java/com/acp/simccs/modules/identity/controller/MfaController.java
+++ simccs/src/main/java/com/acp/simccs/modules/identity/controller/MfaController.java
@@ -7,6 +7,10 @@ import com.acp.simccs.modules.identity.repository.UserRepository;
 import com.warrenstrange.googleauth.GoogleAuthenticator;
 import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
 import com.warrenstrange.googleauth.GoogleAuthenticatorQRGenerator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +29,9 @@ public class MfaController {
 
     private final GoogleAuthenticator gAuth = new GoogleAuthenticator();
 
+    @Operation(summary = "Setup MFA", responses = {
+            @ApiResponse(responseCode = "200", description = "MFA Setup Data", content = @Content(schema = @Schema(implementation = MfaSetupResponse.class)))
+    })
     @GetMapping("/setup")
     public ResponseEntity<Object> setupMfa() {
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -40,6 +47,10 @@ public class MfaController {
         return ResponseEntity.ok(new MfaSetupResponse(secret, qrCodeUrl));
     }
 
+    @Operation(summary = "Verify MFA", responses = {
+            @ApiResponse(responseCode = "200", description = "MFA enabled/verified successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid MFA code")
+    })
     @PostMapping("/verify")
     public ResponseEntity<Object> verifyMfa(@Valid @RequestBody MfaVerifyRequest request) {
         UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
